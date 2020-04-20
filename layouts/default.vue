89385177a176e2ac029134c95964646eaf09d04c
awesome-cats <cattwin82@gmail.com>
2020-04-20 09:30:06 +0900

    init app

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'open Sans', sans-serif;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="posts-page" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .posts-page {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
